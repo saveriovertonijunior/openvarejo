2f4d05cc331bb3dbe097dfe2f527f3b5e41f43c9
suporte@openvarejo.com.br <suporte@openvarejo.com.br@97e29fe1-cfbf-1de6-13ed-0cc351ca01c0>
2012-12-11 05:19:41 +0000

    Primeiros códigos do Projeto. OFramework, OPDV e o inicio de um Mini retaguarda.

---
+++ trunk/Framework/uvariaveisbase.pas
@@ -0,0 +1,16 @@
+unit Uvariaveisbase;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+var
+nomeform : string;
+
+implementation
+
+end.
+
---
+++ trunk/PDV/ufrmsplash.pas
@@ -0,0 +1,26 @@
+unit ufrmSplash;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, UfrmBase;
+
+type
+  TfrmSplash = class(TfrmBase)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  frmSplash: TfrmSplash;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
