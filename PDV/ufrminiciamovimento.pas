4394759f8d867a1a18c335f9149b8920c9290563
suporte@openvarejo.com.br <suporte@openvarejo.com.br@97e29fe1-cfbf-1de6-13ed-0cc351ca01c0>
2012-12-15 15:15:14 +0000

    adicionada tela de iniciamovimento de caixa.

---
+++ PDV/ufrminiciamovimento.pas
@@ -0,0 +1,26 @@
+unit UfrmIniciaMovimento;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, UfrmBase;
+
+type
+  TfrmIniciaMovimento = class(TfrmBase)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  frmIniciaMovimento: TfrmIniciaMovimento;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
