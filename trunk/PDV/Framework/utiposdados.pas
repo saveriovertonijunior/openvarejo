20b86775b08600c9dd56ef8b79a2fc33d8068b3b
suporte@openvarejo.com.br <suporte@openvarejo.com.br@97e29fe1-cfbf-1de6-13ed-0cc351ca01c0>
2013-02-12 18:17:59 +0000

    framework do projeto pdv em lazarus.

---
+++ trunk/PDV/Framework/utiposdados.pas
@@ -0,0 +1,17 @@
+unit Utiposdados;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+// Tipo de estados em que a tela pode está
+type
+  TStatusTela = (stNavegandoGrid, stInserindo, stEditando, stNavegandoEdits);
+
+implementation
+
+end.
+
